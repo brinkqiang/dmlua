f07d050931d5d3ff5e41f9cc4a610828846955ec
brinkqiang <brink.qiang@gmail.com>
2018-04-05 16:35:50 +0800

    fix warning

--- src/tolua/tolua_map.c
+++ src/tolua/tolua_map.c
@@ -25,7 +25,7 @@
 /* Create metatable
     * Create and register new metatable
 */
-static int tolua_newmetatable( lua_State* L, char* name )
+static int tolua_newmetatable( lua_State* L, const char* name )
 {
     int r = luaL_newmetatable( L, name );
 #ifdef LUA_VERSION_NUM /* only lua 5.1 */
