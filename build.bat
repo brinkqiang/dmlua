5f3afc4be5842961dd932a100ff60167c4c36eca
brink.qiang <brink.qiang@gmail.com>
2018-10-31 23:34:54 +0800

    add

--- build.bat
+++ build.bat
@@ -1,5 +1,14 @@
+
+rem - clone code
+rem git clone https://github.com/brinkqiang/dmlua.git
+rem pushd dmlua
+rem git submodule update --init --recursive
+
 rmdir /S /Q build
 mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cd ..
+pushd build
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake --build .
+popd
+
+rem pause
