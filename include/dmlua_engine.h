cc8847fab9ec4fcb1ec69e83f53e39bb7dc22168
brinkqiang <brink.qiang@gmail.com>
2018-12-02 00:25:02 +0800

    add

--- include/dmlua_engine.h
+++ include/dmlua_engine.h
@@ -43,6 +43,226 @@
 #include <mach-o/dyld.h>
 #endif
 
+
+struct lua_nil {};
+// 如果C为true则是A类型，false是B类型
+template <bool C, typename A, typename B>	struct if_ {};
+template <typename A, typename B>			struct if_<true, A, B> { typedef A type; };
+template <typename A, typename B>			struct if_<false, A, B> { typedef B type; };
+
+// 判断是否指针
+template <typename T>
+struct is_ptr { static const bool value = false; };
+template <typename T>
+struct is_ptr<T*> { static const bool value = true; };
+
+// 判断是否是引用
+template<typename T>
+struct is_ref { static const bool value = false; };
+template<typename T>
+struct is_ref<T&> { static const bool value = true; };
+
+// 获取基本类型 指针以及引用
+template <typename T>
+struct base_type { typedef T type; };
+template <typename T>
+struct base_type<T*> { typedef T type; };
+template <typename T>
+struct base_type<T&> { typedef T type; };
+
+// 输入参数转成T类型
+template<typename T>
+struct void2val { static T invoke(void* input) { return *(T*)input; } };
+// 输入参数转成T类型指针
+template<typename T>
+struct void2ptr { static T* invoke(void* input) { return (T*)input; } };
+// 输入参数转成T类型引用
+template<typename T>
+struct void2ref { static T& invoke(void* input) { return *(T*)input; } };
+
+// 将输入参数ptr转换成T T* 或者T&
+template <typename T>
+struct void2type {
+    static T invoke(void* ptr) {
+        return if_<is_ptr<T>::value,
+            void2ptr<typename base_type<T>::type>,
+            typename if_<is_ref<T>::value,
+            void2ref<typename base_type<T>::type>,
+            void2val<typename base_type<T>::type>
+            >::type
+        >::type::invoke(ptr);
+    }
+};
+
+//template <typename T>
+//static T LuaRead(lua_State* L, int index) {
+//    if (CluaTypeid::Instance().get_name<T>()) {
+//        return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
+//    }
+//    else {
+//        return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
+//    }
+//}
+
+//template <>
+//static inline void LuaRead(lua_State* L, int index)
+//{
+//    return;
+//}
+
+//template <>
+//static inline void* LuaRead(lua_State* L, int index)
+//{
+//    return tolua_touserdata(L, index, nullptr);
+//}
+
+//template <>
+//static inline char* LuaRead(lua_State* L, int index)
+//{
+//    return const_cast<char*>(tolua_tostring(L, index, ""));
+//}
+
+//template <>
+//static inline const char* LuaRead(lua_State* L, int index)
+//{
+//    return tolua_tostring(L, index, "");
+//}
+
+//template <>
+//static inline std::string LuaRead(lua_State* L, int index)
+//{
+//    return tolua_tocppstring(L, index, "");
+//}
+
+//template <>
+//static inline int8_t LuaRead(lua_State* L, int index)
+//{
+//    return (int8_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline uint8_t LuaRead(lua_State* L, int index)
+//{
+//    return (uint8_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline int16_t LuaRead(lua_State* L, int index)
+//{
+//    return (int16_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline uint16_t LuaRead(lua_State* L, int index)
+//{
+//    return (uint16_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline int32_t LuaRead(lua_State* L, int index)
+//{
+//    return (int32_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline uint32_t LuaRead(lua_State* L, int index)
+//{
+//    return (uint32_t)tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline int64_t LuaRead<int64_t>(lua_State* L, int index)
+//{
+//    return tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline uint64_t LuaRead<uint64_t>(lua_State* L, int index)
+//{
+//    return tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline bool LuaRead(lua_State* L, int index)
+//{
+//    if (lua_isnil(L, index)) { return false; }
+//    if (lua_isboolean(L, index)) {
+//        return tolua_toboolean(L, index, 0) != 0;
+//    }
+//    else {
+//        return tolua_tonumber(L, index, 0) != 0;
+//    }
+//}
+
+//template <>
+//static inline float LuaRead(lua_State* L, int index)
+//{
+//    return (float)tolua_tonumber(L, index, 0);
+//}
+
+//template <>
+//static inline double LuaRead(lua_State* L, int index)
+//{
+//    return (double)tolua_tonumber(L, index, 0);
+//}
+
+//template <typename T>
+//static T LuaRead(lua_State* L, int index) {
+//    if (CluaTypeid::Instance().get_name<T>()) {
+//        return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
+//    }
+//    else {
+//        return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
+//    }
+//}
+//template <>
+//static inline int64_t LuaRead<int64_t>(lua_State* L, int index)
+//{
+//    return tolua_tointeger(L, index, 0);
+//}
+
+//template <>
+//static inline uint64_t LuaRead<uint64_t>(lua_State* L, int index)
+//{
+//    return tolua_tointeger(L, index, 0);
+//}
+template <typename T>
+struct LuaRead
+{
+    static T Read(lua_State* L, int index)
+    {
+        if (CluaTypeid::Instance().get_name<T>()) {
+            return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
+        }
+        else {
+            return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
+        }
+    }
+};
+
+template <>
+struct LuaRead<int64_t>
+{
+    static int64_t Read(lua_State* L, int index)
+    {
+        return tolua_tointeger(L, index, 0);
+    }
+};
+
+template <>
+struct LuaRead<uint64_t>
+{
+    static uint64_t Read(lua_State* L, int index)
+    {
+        return tolua_tointeger(L, index, 0);
+    }
+};
+template <typename T>
+static inline T LuaPop(lua_State* L) { T ret = LuaRead<T>::Read(L, -1); lua_pop(L, 1); return ret; }
+
+static inline void LuaPop(lua_State* L) { lua_pop(L, 1); }
+
+
 TOLUA_API int tolua_interface_open( lua_State* tolua_S );
 
 inline char* tolua_SafeStrCopy( char* des, const char* src, size_t max_len ) {
@@ -451,225 +671,6 @@ FAIL:
 
         return 0;
     }
-
-    struct lua_nil {};
-    // 如果C为true则是A类型，false是B类型
-    template <bool C, typename A, typename B>	struct if_ {};
-    template <typename A, typename B>			struct if_<true, A, B> { typedef A type; };
-    template <typename A, typename B>			struct if_<false, A, B> { typedef B type; };
-
-    // 判断是否指针
-    template <typename T>
-    struct is_ptr { static const bool value = false; };
-    template <typename T>
-    struct is_ptr<T*> { static const bool value = true; };
-
-    // 判断是否是引用
-    template<typename T>
-    struct is_ref { static const bool value = false; };
-    template<typename T>
-    struct is_ref<T&> { static const bool value = true; };
-
-    // 获取基本类型 指针以及引用
-    template <typename T>
-    struct base_type { typedef T type; };
-    template <typename T>
-    struct base_type<T*> { typedef T type; };
-    template <typename T>
-    struct base_type<T&> { typedef T type; };
-
-    // 输入参数转成T类型
-    template<typename T>
-    struct void2val { static T invoke(void* input) { return *(T*)input; } };
-    // 输入参数转成T类型指针
-    template<typename T>
-    struct void2ptr { static T* invoke(void* input) { return (T*)input; } };
-    // 输入参数转成T类型引用
-    template<typename T>
-    struct void2ref { static T& invoke(void* input) { return *(T*)input; } };
-
-    // 将输入参数ptr转换成T T* 或者T&
-    template <typename T>
-    struct void2type {
-        static T invoke(void* ptr) {
-            return if_<is_ptr<T>::value,
-                void2ptr<typename base_type<T>::type>,
-                typename if_<is_ref<T>::value,
-                void2ref<typename base_type<T>::type>,
-                void2val<typename base_type<T>::type>
-                >::type
-            >::type::invoke(ptr);
-        }
-    };
-
-    //template <typename T>
-    //static T LuaRead(lua_State* L, int index) {
-    //    if (CluaTypeid::Instance().get_name<T>()) {
-    //        return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
-    //    }
-    //    else {
-    //        return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
-    //    }
-    //}
-
-    //template <>
-    //static inline void LuaRead(lua_State* L, int index)
-    //{
-    //    return;
-    //}
-
-    //template <>
-    //static inline void* LuaRead(lua_State* L, int index)
-    //{
-    //    return tolua_touserdata(L, index, nullptr);
-    //}
-
-    //template <>
-    //static inline char* LuaRead(lua_State* L, int index)
-    //{
-    //    return const_cast<char*>(tolua_tostring(L, index, ""));
-    //}
-
-    //template <>
-    //static inline const char* LuaRead(lua_State* L, int index)
-    //{
-    //    return tolua_tostring(L, index, "");
-    //}
-
-    //template <>
-    //static inline std::string LuaRead(lua_State* L, int index)
-    //{
-    //    return tolua_tocppstring(L, index, "");
-    //}
-
-    //template <>
-    //static inline int8_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (int8_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline uint8_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (uint8_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline int16_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (int16_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline uint16_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (uint16_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline int32_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (int32_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline uint32_t LuaRead(lua_State* L, int index)
-    //{
-    //    return (uint32_t)tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline int64_t LuaRead<int64_t>(lua_State* L, int index)
-    //{
-    //    return tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline uint64_t LuaRead<uint64_t>(lua_State* L, int index)
-    //{
-    //    return tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline bool LuaRead(lua_State* L, int index)
-    //{
-    //    if (lua_isnil(L, index)) { return false; }
-    //    if (lua_isboolean(L, index)) {
-    //        return tolua_toboolean(L, index, 0) != 0;
-    //    }
-    //    else {
-    //        return tolua_tonumber(L, index, 0) != 0;
-    //    }
-    //}
-
-    //template <>
-    //static inline float LuaRead(lua_State* L, int index)
-    //{
-    //    return (float)tolua_tonumber(L, index, 0);
-    //}
-
-    //template <>
-    //static inline double LuaRead(lua_State* L, int index)
-    //{
-    //    return (double)tolua_tonumber(L, index, 0);
-    //}
-
-    //template <typename T>
-    //static T LuaRead(lua_State* L, int index) {
-    //    if (CluaTypeid::Instance().get_name<T>()) {
-    //        return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
-    //    }
-    //    else {
-    //        return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
-    //    }
-    //}
-    //template <>
-    //static inline int64_t LuaRead<int64_t>(lua_State* L, int index)
-    //{
-    //    return tolua_tointeger(L, index, 0);
-    //}
-
-    //template <>
-    //static inline uint64_t LuaRead<uint64_t>(lua_State* L, int index)
-    //{
-    //    return tolua_tointeger(L, index, 0);
-    //}
-    template <typename T>
-    struct LuaRead
-    {
-        static T Read(lua_State* L, int index)
-        {
-            if (CluaTypeid::Instance().get_name<T>()) {
-                return void2type<T>::invoke(tolua_tousertype(L, index, nullptr));
-            }
-            else {
-                return void2type<T>::invoke(tolua_touserdata(L, index, nullptr));
-            }
-        }
-    };
-
-    template <>
-    struct LuaRead<int64_t>
-    {
-        static int64_t Read(lua_State* L, int index)
-        {
-            return tolua_tointeger(L, index, 0);
-        }
-    };
-
-    template <>
-    struct LuaRead<uint64_t>
-    {
-        static uint64_t Read(lua_State* L, int index)
-        {
-            return tolua_tointeger(L, index, 0);
-        }
-    };
-    template <typename T>
-    static inline T LuaPop(lua_State* L) { T ret = LuaRead<T>::Read(L, -1); lua_pop(L, 1); return ret; }
-
-    static inline void LuaPop(lua_State* L) { lua_pop(L, 1); }
-
 
     template<typename T>
     static inline void PushLuaParam( lua_State* luaS, T& t ) {
