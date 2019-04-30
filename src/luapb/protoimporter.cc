9820cfd4271516365dc849327e27a96b97706dd8
brinkqiang <brink.qiang@gmail.com>
2019-04-30 16:22:14 +0800

    add

--- src/luapb/protoimporter.cc
+++ src/luapb/protoimporter.cc
@@ -4,15 +4,12 @@
 #include <stdio.h>
 #include "dmutil.h"
 
-class ProtobufLibrary
-{
-public:
-    ProtobufLibrary()
-    {
+class ProtobufLibrary {
+  public:
+    ProtobufLibrary() {
         GOOGLE_PROTOBUF_VERIFY_VERSION;
     };
-    virtual ~ProtobufLibrary()
-    {
+    virtual ~ProtobufLibrary() {
         // fixed memory leak
         google::protobuf::ShutdownProtobufLibrary();
     }
@@ -20,85 +17,103 @@ public:
 
 static ProtobufLibrary _protobuf_library;
 
-ProtoImporterImpl::ProtoImporterImpl(MyMultiFileErrorCollector& oErrorCollector, google::protobuf::compiler::DiskSourceTree& oSourceTree):
-		m_oImporter(&oSourceTree, &oErrorCollector)
-{
+class MyMultiFileErrorCollector : public
+    google::protobuf::compiler::MultiFileErrorCollector {
+  public:
+    virtual void AddError(const std::string& filename, int line, int column,
+                          const std::string& message) {
+        fprintf(stderr, "%s:%d:%d:%s\n", filename.c_str(), line, column,
+                message.c_str());
+    }
+
+    virtual void AddWarning(const std::string& filename, int line, int column,
+                            const std::string& message) {
+        fprintf(stdout, "%s:%d:%d:%s\n", filename.c_str(), line, column,
+                message.c_str());
+    }
+};
+
+static MyMultiFileErrorCollector errorCollector;
+static google::protobuf::compiler::DiskSourceTree sourceTree;
+
+ProtoImporterImpl::ProtoImporterImpl():
+    m_oImporter(&sourceTree, &errorCollector) {
 
 }
 
-ProtoImporterImpl::~ProtoImporterImpl()
-{
+ProtoImporterImpl::~ProtoImporterImpl() {
 }
 
-bool ProtoImporterImpl::Import(const std::string& strFileName)
-{
-	const  google::protobuf::FileDescriptor* filedescriptor = m_oImporter.Import(strFileName);
-	if (!filedescriptor)
-	{
-		fprintf(stderr, "import (%s) file descriptor error\n", strFileName.c_str());
-		return false;
-	}
-	return true;
+bool ProtoImporterImpl::Import(const std::string& strFileName) {
+    const  google::protobuf::FileDescriptor* filedescriptor = m_oImporter.Import(
+                strFileName);
+
+    if (!filedescriptor) {
+        fprintf(stderr, "import (%s) file descriptor error\n", strFileName.c_str());
+        return false;
+    }
+
+    return true;
 }
 
-google::protobuf::Message* ProtoImporterImpl::CreateMessage(const std::string& strTypeName)
-{
-	google::protobuf::Message* message = NULL;
-	const google::protobuf::Descriptor* descriptor = m_oImporter.pool()->FindMessageTypeByName(strTypeName);
-	if (descriptor)
-	{
-		const google::protobuf::Message* prototype = m_oFactory.GetPrototype(descriptor);
-		if (prototype)
-		{
-			message = prototype->New();
-		}
-	}
-	return message;
+google::protobuf::Message* ProtoImporterImpl::CreateMessage(
+    const std::string& strTypeName) {
+    google::protobuf::Message* message = NULL;
+    const google::protobuf::Descriptor* descriptor =
+        m_oImporter.pool()->FindMessageTypeByName(strTypeName);
+
+    if (descriptor) {
+        const google::protobuf::Message* prototype = m_oFactory.GetPrototype(
+                    descriptor);
+
+        if (prototype) {
+            message = prototype->New();
+        }
+    }
+
+    return message;
 }
 
-ProtoImporter::ProtoImporter()
-{
+ProtoImporter::ProtoImporter() {
     std::string strRoot = DMGetRootPath();
     std::string strProtoPath = strRoot + PATH_DELIMITER_STR + "proto";
-    std::string strProtoPath2 = strRoot + PATH_DELIMITER_STR + ".." + PATH_DELIMITER_STR + "proto";
+    std::string strProtoPath2 = strRoot + PATH_DELIMITER_STR + ".." +
+                                PATH_DELIMITER_STR + "proto";
 
-    m_oSourceTree.MapPath("", strRoot);
-    m_oSourceTree.MapPath("", strProtoPath);
-    m_oSourceTree.MapPath("", strProtoPath2);
+    sourceTree.MapPath("", strRoot);
+    sourceTree.MapPath("", strProtoPath);
+    sourceTree.MapPath("", strProtoPath2);
 
     printf("[ProtoImporter] protopath:%s\n", strRoot.c_str());
     printf("[ProtoImporter] protopath:%s\n", strProtoPath.c_str());
     printf("[ProtoImporter] protopath:%s\n", strProtoPath2.c_str());
- 
-    SetImporter(new ProtoImporterImpl(m_oErrorCollector, m_oSourceTree));
+    SetImporter(new ProtoImporterImpl());
 }
 
-ProtoImporter::~ProtoImporter()
-{
+ProtoImporter::~ProtoImporter() {
 
 }
 
-bool ProtoImporter::Import(const std::string& strFileName)
-{
+bool ProtoImporter::Import(const std::string& strFileName) {
     ProtoImporterImpl* poProtoImporter = GetImporter();
-    if (NULL != poProtoImporter)
-    {
+
+    if (NULL != poProtoImporter) {
         delete poProtoImporter;
         SetImporter(NULL);
     }
 
-    SetImporter(new ProtoImporterImpl(m_oErrorCollector, m_oSourceTree));
+    SetImporter(new ProtoImporterImpl());
     poProtoImporter = GetImporter();
- 
-    if (NULL == poProtoImporter)
-    {
+
+    if (NULL == poProtoImporter) {
+        fprintf(stderr, "import (%s) file descriptor Importer is nil\n", strFileName.c_str());
         return false;
     }
 
     return poProtoImporter->Import(strFileName);
 }
 
-google::protobuf::Message* ProtoImporter::CreateMessage(const std::string& strTypeName)
-{
+google::protobuf::Message* ProtoImporter::CreateMessage(
+    const std::string& strTypeName) {
     return m_poProtoImporter->CreateMessage(strTypeName);
 }
--- src/luapb/protoimporter.h
+++ src/luapb/protoimporter.h
@@ -9,41 +9,21 @@
 #include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/compiler/importer.h>
 
-class MyMultiFileErrorCollector : public google::protobuf::compiler::MultiFileErrorCollector
-{
-public:
-    virtual void AddError(const std::string& filename, int line, int column,
-        const std::string& message)
-    {
-        fprintf(stderr, "%s:%d:%d:%s\n", filename.c_str(), line, column, message.c_str());
-    }
-
-    virtual void AddWarning(const std::string& filename, int line, int column,
-        const std::string& message)
-    {
-        fprintf(stdout, "%s:%d:%d:%s\n", filename.c_str(), line, column, message.c_str());
-    }
-};
-
-class ProtoImporterImpl
-{
-public:
-	ProtoImporterImpl(MyMultiFileErrorCollector& oErrorCollector, google::protobuf::compiler::DiskSourceTree& oSourceTree);
+class ProtoImporterImpl {
+  public:
+    ProtoImporterImpl();
     virtual ~ProtoImporterImpl();
-public:
-	bool Import(const std::string& strFileName);
-	google::protobuf::Message* CreateMessage(const std::string& strTypeName);
-public:
-	google::protobuf::compiler::Importer m_oImporter;
-	google::protobuf::DynamicMessageFactory m_oFactory;
-
-
+  public:
+    bool Import(const std::string& strFileName);
+    google::protobuf::Message* CreateMessage(const std::string& strTypeName);
+  public:
+    google::protobuf::compiler::Importer m_oImporter;
+    google::protobuf::DynamicMessageFactory m_oFactory;
 };
 
-class ProtoImporter : public TSingleton<ProtoImporter>
-{
+class ProtoImporter : public TSingleton<ProtoImporter> {
     friend class TSingleton<ProtoImporter>;
-public:
+  public:
     ProtoImporter();
     virtual ~ProtoImporter();
 
@@ -58,13 +38,8 @@ public:
         m_poProtoImporter = poProtoImporter;
     }
 
-    MyMultiFileErrorCollector& GetErrorCollector() { return m_oErrorCollector; }
-    google::protobuf::compiler::DiskSourceTree& GetSourceTree() { return m_oSourceTree; }
-private:
+  private:
     ProtoImporterImpl* m_poProtoImporter;
-
-    MyMultiFileErrorCollector m_oErrorCollector;
-    google::protobuf::compiler::DiskSourceTree m_oSourceTree;
 };
 
 
