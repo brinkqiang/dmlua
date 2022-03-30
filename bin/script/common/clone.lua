545704de54c23a99f60453fe54795ed8291d004b
brinkqiang <brink.qiang@gmail.com>
2022-03-30 15:36:06 +0800

    add

---
+++ bin/script/common/clone.lua
@@ -0,0 +1,28 @@
+-- lua script
+module (..., package.seeall)
+
+function clone( object )
+    local lookup_table = {}
+    local function copyObj( object )
+        if type( object ) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        
+        local new_table = {}
+        lookup_table[object] = new_table
+        for key, value in pairs( object ) do
+            new_table[copyObj( key )] = copyObj( value )
+        end
+        return setmetatable( new_table, getmetatable( object ) )
+    end
+    return copyObj( object )
+end
+
+function main()
+    local map = { id=1, count=2}
+    local map2 = clone(map)
+    print(map)
+    print(map2)
+end
