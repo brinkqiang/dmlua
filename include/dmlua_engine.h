9f24c382816025d4a50faebdcc3507a2c7c21ab2
brinkqiang <brink.qiang@gmail.com>
2025-07-21 22:40:19 +0800

    add

--- include/dmlua_engine.h
+++ include/dmlua_engine.h
@@ -474,24 +474,6 @@ struct LuaReader<uint32_t>
     }
 };
 
-//template <>
-//struct LuaReader<long>
-//{
-//    static inline long Read(lua_State* L, int index)
-//    {
-//        return (long)tolua_tointeger(L, index, 0);
-//    }
-//};
-//
-//template <>
-//struct LuaReader<unsigned long>
-//{
-//    static inline unsigned long Read(lua_State* L, int index)
-//    {
-//        return (unsigned long)tolua_tointeger(L, index, 0);
-//    }
-//};
-
 template <>
 struct LuaReader<int64_t>
 {
